f44359e09f9ef00fd3ffb6cd3c0268ce22f319e2
曾曼青 <2192073703@qq.com>
2025-06-05 14:36:50 +0800

    Update lint.js

--- lint.js
+++ lint.js
@@ -7,3 +7,7 @@ const add2 = function(a, b) {
 const equal = function(a, b) {
     return a == b;
 }
+const request = async function() {
+    const res = await axios.get('/test');
+    return res;
+}
